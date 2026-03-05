fbd63599188f4da69e81e4332bbacecd442a325b
Jimmy20060109 <hdsteeler0@gmail.com>
2026-03-05 03:23:48 -0500

    add analytics in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { routes } from './routes'
 import './index.css'
@@ -9,5 +10,6 @@ const router = createBrowserRouter(routes)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
 )
